97780c505390b91a408a06bc36b7a3dce9b456c6
umutdev <umutsayar8@gmail.com>
2022-10-21 21:15:40 +0300

    duzenleme

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -20,6 +20,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
     textController1 = TextEditingController();
     textController2 = TextEditingController();
   }
+
   UnfocusDisposition disposition = UnfocusDisposition.scope;
   @override
   Widget build(BuildContext context) {
@@ -34,47 +35,46 @@ class _HomePageWidgetState extends State<HomePageWidget> {
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(30, 40, 30, 0),
+                padding: const EdgeInsetsDirectional.fromSTEB(30, 40, 30, 0),
                 child: TextFormField(
                   controller: textController1,
                   obscureText: false,
                   decoration: InputDecoration(
-                    labelText: 'Ham Değer :',
-                    enabledBorder: UnderlineInputBorder(
+                    labelText: 'İlk değeri giriniz:',
+                    enabledBorder: const UnderlineInputBorder(
                       borderSide: BorderSide(
                         color: Colors.black,
                         width: 2,
                       ),
-                      borderRadius: const BorderRadius.only(
+                      borderRadius: BorderRadius.only(
                         topLeft: Radius.circular(4.0),
                         topRight: Radius.circular(4.0),
                       ),
                     ),
-                    focusedBorder: UnderlineInputBorder(
+                    focusedBorder: const UnderlineInputBorder(
                       borderSide: BorderSide(
                         color: Colors.black,
                         width: 2,
                       ),
-                      borderRadius: const BorderRadius.only(
+                      borderRadius: BorderRadius.only(
                         topLeft: Radius.circular(4.0),
                         topRight: Radius.circular(4.0),
                       ),
                     ),
                     suffixIcon: textController1.text.isNotEmpty
                         ? InkWell(
-                      onTap: () =>
-                          setState(
-                                () => textController1.clear(),
-                          ),
-                      child: Icon(
-                        Icons.clear,
-                        color: Color(0xFF757575),
-                        size: 20,
-                      ),
-                    )
+                            onTap: () => setState(
+                              () => textController1.clear(),
+                            ),
+                            child: const Icon(
+                              Icons.clear,
+                              color: Color(0xFF757575),
+                              size: 20,
+                            ),
+                          )
                         : null,
                   ),
-                  style: TextStyle(
+                  style: const TextStyle(
                     fontFamily: 'Poppins',
                     fontSize: 30,
                   ),
@@ -83,60 +83,58 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                 ),
               ),
               Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(30, 40, 30, 0),
+                padding: const EdgeInsetsDirectional.fromSTEB(30, 40, 30, 0),
                 child: Row(
                   mainAxisSize: MainAxisSize.max,
                   children: [
                     Expanded(
                       child: Padding(
-                        padding: EdgeInsetsDirectional.fromSTEB(0, 0, 30, 0),
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(0, 0, 30, 0),
                         child: TextFormField(
-                          onChanged: (_) =>
-                              EasyDebounce.debounce(
-                                'textController2',
-                                Duration(milliseconds: 2000),
-                                    () => setState(() {}),
-                              ),
+                          onChanged: (_) => EasyDebounce.debounce(
+                            'textController2',
+                            const Duration(milliseconds: 2000),
+                            () => setState(() {}),
+                          ),
                           controller: textController2,
                           obscureText: false,
                           decoration: InputDecoration(
-                            labelText: 'Yüzde kaçı ?',
-                            enabledBorder: UnderlineInputBorder(
+                            labelText: 'Son değeri giriniz',
+                            enabledBorder: const UnderlineInputBorder(
                               borderSide: BorderSide(
                                 color: Colors.black,
                                 width: 2,
                               ),
-                              borderRadius: const BorderRadius.only(
+                              borderRadius: BorderRadius.only(
                                 topLeft: Radius.circular(4.0),
                                 topRight: Radius.circular(4.0),
                               ),
                             ),
-                            focusedBorder: UnderlineInputBorder(
+                            focusedBorder: const UnderlineInputBorder(
                               borderSide: BorderSide(
                                 color: Colors.black,
                                 width: 2,
                               ),
-                              borderRadius: const BorderRadius.only(
+                              borderRadius: BorderRadius.only(
                                 topLeft: Radius.circular(4.0),
                                 topRight: Radius.circular(4.0),
                               ),
                             ),
                             suffixIcon: textController2.text.isNotEmpty
                                 ? InkWell(
-                              onTap: () =>
-                                  setState(
-                                        () => textController2.clear(),
-                                  ),
-                              child: Icon(
-                                Icons.clear,
-                                color: Color(0xFF757575),
-                                size: 20,
-                              ),
-                            )
+                                    onTap: () => setState(
+                                      () => textController2.clear(),
+                                    ),
+                                    child: const Icon(
+                                      Icons.clear,
+                                      color: Color(0xFF757575),
+                                      size: 20,
+                                    ),
+                                  )
                                 : null,
                           ),
-                          style:
-                          TextStyle(
+                          style: const TextStyle(
                             fontFamily: 'Poppins',
                             fontSize: 30,
                           ),
@@ -145,7 +143,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                         ),
                       ),
                     ),
-                    FaIcon(
+                    const FaIcon(
                       FontAwesomeIcons.percent,
                       color: Colors.black,
                       size: 40,
@@ -159,27 +157,28 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(30, 0, 30, 40),
+                      padding:
+                          const EdgeInsetsDirectional.fromSTEB(30, 0, 30, 40),
                       child: ElevatedButton(
                         onPressed: () {
                           setState(() {
                             primaryFocus.unfocus(disposition: disposition);
                           });
-                          Calculate();
-                          CalculateDiscount();
+                          calculate();
+                          calculateDiscount();
                           bottomSheet(context);
                         },
-                        child: Text("Hesapla"),
+                        child: const Text("Hesapla"),
                         style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all<Color>(
-                              Colors.purple),
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.purple),
                           shape: MaterialStateProperty.all(
                             RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(12),
                             ),
                           ),
-                          fixedSize: MaterialStateProperty.all(const Size(
-                              4000, 40)),
+                          fixedSize:
+                              MaterialStateProperty.all(const Size(4000, 40)),
                           textStyle: MaterialStateProperty.all(
                             const TextStyle(
                                 fontSize: 17, fontFamily: 'Poppins'),
@@ -196,37 +195,41 @@ class _HomePageWidgetState extends State<HomePageWidget> {
       ),
     );
   }
-  void Calculate(){
-    num a = double.parse(textController1.text);
-    num b = double.parse(textController2.text);
-    sonuc = (a * (b/100));
+
+  void calculate() {
+    num a = double.parse(textController1.text); // first value
+    num b = double.parse(textController2.text); // last value
+    sonuc = ((b - a) / a * 100);
   }
-  void CalculateDiscount(){
+
+  void calculateDiscount() {
     num a = double.parse(textController1.text);
     indirim = a - sonuc;
   }
 }
+
 num sonuc, indirim;
-  bottomSheet(context) {
-    showModalBottomSheet(context: context,
-        builder: (BuildContext c) {
-          return Wrap(
-              children: <Widget>[Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsetsDirectional.fromSTEB(30, 50, 70, 50),
-                    child: Text("Sonuç: ${sonuc.toStringAsFixed(2)} \n\nİndirimli Hali: ${indirim.toStringAsFixed(2)}",
-                      style: TextStyle(
-                        fontSize: 30.0,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
+bottomSheet(context) {
+  showModalBottomSheet(
+      context: context,
+      builder: (BuildContext c) {
+        return Wrap(children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(30, 50, 70, 50),
+                child: Text(
+                  "Zam oranı %: ${sonuc.toStringAsFixed(2)}",
+                  style: const TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.w600,
                   ),
-                ],
+                ),
               ),
-              ]
-          );
-        });
-  }
+            ],
+          ),
+        ]);
+      });
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,9 @@ import '../homepage.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key : key);
+  const MyApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: HomePageWidget(),
+      home: const HomePageWidget(),
     );
   }
 }
